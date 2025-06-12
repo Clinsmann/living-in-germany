3a290cbef8648cbdda57986ea0f4293595b6fb21
clinsmann <ibeanuhillary@gmail.com>
2025-06-12 19:43:22 +0200

    Refactor SidebarNavigation component to implement a bottom sheet layout with smooth animations, enhance button interactions, and improve accessibility. Update Header component layout for better alignment and styling. Add germanStates array to manage state selection.

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -8,22 +8,20 @@ export default function Header({
     toggleSettings: () => void
 }) {
     return (
-        <header className="mb-8 flex flex-row items-center gap-4 justify-between">
-            <div className="flex flex-row items-center gap-4">
-                <div className="rounded-md overflow-hidden">
-                    <Image src={flag} alt="deutschland flag" width={48} />
+        <header className="mb-8 flex flex-col items-center gap-4">
+            <div className="flex flex-row items-center gap-4 justify-between w-full">
+                <div className="rounded-md overflow-hidden shadow-md">
+                    <Image src={flag} alt="deutschland flag" width={50} />
                 </div>
-                <div className="flex flex-col">
-                    <h1 className="text-md font-bold text-gray-900 dark:text-white mb-1">
-                        Einbürgerungstest Deutsch
-                    </h1>
-                    <p className="text-gray-600 dark:text-gray-400 text-sm">Master your citizenship test preparation</p>
-                </div>
-            </div>
-            <div className="flex flex-row gap-2">
-                <button className="text-white p-3 rounded-md dark:bg-gray-800" onClick={toggleSettings}>
+
+                <button className="text-gray-900 p-4 rounded-md dark:bg-gray-800 bg-gray-200 dark:text-white shadow-md" onClick={toggleSettings}>
                     <Settings className="size-5" />
                 </button>
             </div>
+
+            <h1 className="text-md font-bold text-gray-900 dark:text-white mb-1">
+                Einbürgerungstest Deutsch
+            </h1>
+            <p className="text-gray-600 dark:text-gray-400 text-sm">Master your citizenship test preparation</p>
         </header>)
 }
